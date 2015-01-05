f9e931e251504d4ed7d7f600f9743a26d5844d09
ShareArch <whtoo@qq.com>
2015-01-05 10:51:58 +0800

    增加访问设备监控--spring mobile

--- src/java/com/fxweixin/index/web/IndexAction.java
+++ src/java/com/fxweixin/index/web/IndexAction.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.fxweixin.index.web;
 
 import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.mobile.device.Device;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,16 +22,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("welcome")
 public class IndexAction {
+
     private final static Logger LOG = LoggerFactory.getLogger(IndexAction.class);
-    
-     @RequestMapping(method = RequestMethod.GET)
-     public String welcome(Model model) {
-        String flag = "mobile/index/index";
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String welcome(Model model, Device device) {
+        String flag = "pc/index/index";
+        if (device.isMobile()) {
+            flag = "mobile/index/index";
+        }
         Date date = new Date();
-        model.addAttribute("date",date );
+        model.addAttribute("date", date);
         return flag;
-     }
-     
+    }
+
     /**
      *
      * @param useremail
@@ -39,21 +43,22 @@ public class IndexAction {
      * @param model
      * @return
      */
-    @RequestMapping(value="/login",method = RequestMethod.POST)
-     public String login(@RequestParam String useremail,@RequestParam String pwd,Model model) {
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(@RequestParam String useremail, @RequestParam String pwd, Model model) {
         String flag = "pc/index/index";
-        LOG.info("email "+useremail);
-        LOG.info("pwd "+pwd);
-        if(!"whtoo@qq.com".equals(useremail)){
+        LOG.info("email " + useremail);
+        LOG.info("pwd " + pwd);
+        if (!"whtoo@qq.com".equals(useremail)) {
             flag = "redirect:/welcome/register";
         }
         return flag;
-     }
-     @RequestMapping(value="/register",method = RequestMethod.GET)
-     public String register(){
-          String flag = "pc/login/register";
-          
-          return flag;
-     }
-     
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String register() {
+        String flag = "pc/login/register";
+
+        return flag;
+    }
+
 }
